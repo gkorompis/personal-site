3f86ab5737518fb73750d383187480780336c1ab
gkorompis <gracia.korompis@alumni.i3l.ac.id>
2023-06-16 15:28:58 +0700

    final version

--- contacts/contacts.js
+++ contacts/contacts.js
@@ -1,18 +1,16 @@
 console.log("hallo!");
 
-let changeImage = (id) =>{
-    let img = document.getElementById(id);
-    let src = img.getAttribute('src').replace(/\.jpg/g, "");
-    let index = src[+src.length - 1];
-    // let index = id[+id.length - 1];
-    let randomInt = Math.floor(Math.random()*10);
-    randomInt = randomInt == +index ? randomInt+1 : randomInt;
-    let source = `./assets/img${randomInt}.jpg`;
-    img.classList.add('fade-in')
-    img.setAttribute('src', source)
-    img.classList.remove('fade-in')
-    console.log('hidden')
-}
+
+// let postComment = (input) =>{
+//     let comment = input || "no comment!";
+//     console.log(comment);
+//     input ? alert(`"${comment}" is posted!`) : alert("no comment!");
+//     // if(input){
+//     //     newBox = document.createElement('div');
+//     //     document.getElementById('list-comment-box').append('n')
+//     // }
+//     return comment;
+// }
 
 let postComment =(event)=> {
             event.preventDefault();
@@ -21,13 +19,17 @@ let postComment =(event)=> {
             console.log(comment)
             if (comment) {
                 console.log('comment')
+                let d = new Date();
+                let currentTime = d.toLocaleTimeString();
                 const commentList = document.getElementById('comment-list');
                 const newComment = document.createElement('div');
                 newComment.className = 'comment';
-                newComment.innerHTML = comment;
+                newComment.innerHTML = `${currentTime}: ${comment}`;
                 commentList.prepend(newComment);
                 commentInput.value = '';
             }
+           
+            
             comment ? alert(`"${comment}" is posted!`) : alert("no comment!");
         }
 
@@ -35,6 +37,7 @@ let cards = document.querySelector('.card-list').childNodes;
 cards.forEach(card =>{
     card.addEventListener('click', (event)=>{
         console.log('clicked', event.target.getAttribute("id"));
+        // hideCard(event.target.getAttribute("id"));
         changeImage(event.target.getAttribute("alt"))
     })
 })
